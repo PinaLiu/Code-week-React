20c6c90bc0876a764fb2bf4c8a0c84c819218276
PinaLiu <119705587+PinaLiu@users.noreply.github.com>
2024-03-27 12:20:38 +0100

    elimino server.js

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -1,14 +1,44 @@
-export default function handler(req, res) {
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const orderSchema = new Schema({
+  nome: String,
+  pizza: String,
+  base: String,
+  impasto: String,
+  ingredienti: [String],
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+// Connessione a MongoDB
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { nome, pizza, base, impasto, ingredienti } = req.body;
-    const newOrder = {
-      nome,
-      pizza,
-      base,
-      impasto,
-      ingredienti,
-    };
-    res.status(200).json({ message: "Ordine ricevuto con successo!" });
+
+    // Connessione a MongoDB
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Crea un nuovo ordine utilizzando il modello Order
+    const newOrder = new Order({ nome, pizza, base, impasto, ingredienti });
+
+    // Salva l'ordine nel database
+    newOrder
+      .save()
+      .then(() => {
+        console.log("Ordine salvato nel database:", newOrder);
+        res.status(200).json({ message: "Ordine ricevuto con successo!" });
+      })
+      .catch((error) => {
+        console.error("Errore durante il salvataggio dell'ordine:", error);
+        res.status(500).json({
+          error:
+            "Si è verificato un errore durante il salvataggio dell'ordine.",
+        });
+      });
   } else {
     res.status(405).end();
   }
