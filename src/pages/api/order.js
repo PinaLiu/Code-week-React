3957624e810c25e027e2a251a56a5ddb3438c500
PinaLiu <119705587+PinaLiu@users.noreply.github.com>
2024-03-27 11:25:46 +0100

    elimino server.js con express del cavolo

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -1,18 +1,34 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    // Gestione della richiesta POST
-    // Esempio: Salvataggio dell'ordine nel database
-    const { nome, pizza, base, impasto, ingredienti } = req.body;
-    // Implementa qui la logica per salvare l'ordine nel database
+import mongoose from "mongoose";
 
-    // Esempio di logica di salvataggio (da adattare al tuo caso specifico)
-    const newOrder = {
+// Definisci lo schema del modello per gli ordini
+const Schema = mongoose.Schema;
+const orderSchema = new Schema({
+  nome: String,
+  pizza: String, //riferito alle nostre pizze
+  //crea la tua pizza
+  base: String,
+  impasto: String,
+  ingredienti: [String],
+});
+const Order = mongoose.model("Order", orderSchema);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { nome, pizza, base, impasto, ingredienti } = req.body;
+    const newOrder = new Order({
       nome,
       pizza,
       base,
       impasto,
       ingredienti,
-    };
+    });
+
+    // Connessione a MongoDB
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     // Salva l'ordine nel database
     newOrder
       .save()
@@ -20,12 +36,10 @@ export default function handler(req, res) {
         res.status(200).json({ message: "Ordine ricevuto con successo!" });
       })
       .catch((error) => {
-        res
-          .status(500)
-          .json({
-            error:
-              "Si è verificato un errore durante il salvataggio dell'ordine.",
-          });
+        res.status(500).json({
+          error:
+            "Si è verificato un errore durante il salvataggio dell'ordine.",
+        });
       });
   } else {
     res.status(405).end();
