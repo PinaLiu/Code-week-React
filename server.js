55f1236f821ea2837d63c76fc9b3c6895b30fe74
PinaLiu <119705587+PinaLiu@users.noreply.github.com>
2024-03-27 12:09:11 +0100

    ricominciamo da express

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// Parser per i dati della richiesta
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// Connessione a MongoDB
+mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connessione a MongoDB riuscita"))
+  .catch((err) =>
+    console.error("Errore durante la connessione a MongoDB:", err)
+  );
+
+// Definisci lo schema del modello per gli ordini
+const Schema = mongoose.Schema;
+const orderSchema = new Schema({
+  nome: String,
+  pizza: String, //riferito alle nostre pizze
+  //crea la tua pizza
+  base: String,
+  impasto: String,
+  ingredienti: [String],
+});
+const Order = mongoose.model("Order", orderSchema);
+
+// Rotte
+app.post("/api/order", (req, res) => {
+  const { nome, pizza, base, impasto, ingredienti } = req.body;
+
+  // Crea un nuovo ordine utilizzando il modello Order
+  const newOrder = new Order({ nome, pizza, base, impasto, ingredienti });
+
+  // Salva l'ordine nel database
+  newOrder
+    .save()
+    .then(() => {
+      console.log("Ordine salvato nel database:", newOrder);
+      res.status(200).json({ message: "Ordine ricevuto con successo!" });
+    })
+    .catch((error) => {
+      console.error("Errore durante il salvataggio dell'ordine:", error);
+      res.status(500).json({
+        error: "Si è verificato un errore durante il salvataggio dell'ordine.",
+      });
+    });
+});
+app.listen(PORT, () => {
+  console.log(`Server in ascolto sulla porta ${PORT}`);
+});
--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -1,48 +1,14 @@
-import mongoose from "mongoose";
-
-// Definisci lo schema del modello per gli ordini
-const Schema = mongoose.Schema;
-const orderSchema = new Schema({
-  nome: String,
-  pizza: String, //riferito alle nostre pizze
-  //crea la tua pizza
-  base: String,
-  impasto: String,
-  ingredienti: [String],
-});
-const Order = mongoose.model("Order", orderSchema);
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method === "POST") {
     const { nome, pizza, base, impasto, ingredienti } = req.body;
-    const newOrder = new Order({
+    const newOrder = {
       nome,
       pizza,
       base,
       impasto,
       ingredienti,
-    });
-
-    // Connessione a MongoDB
-    await mongoose.connect(process.env.DATABASE_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    // Salva l'ordine nel database
-    newOrder
-      .save()
-      .then(() => {
-        res.status(200).json({ message: "Ordine ricevuto con successo!" });
-        mongoose.connection.close(); // Chiudi la connessione
-      })
-      .catch((error) => {
-        res.status(500).json({
-          error:
-            "Si è verificato un errore durante il salvataggio dell'ordine.",
-        });
-        mongoose.connection.close(); // Chiudi la connessione
-      });
+    };
+    res.status(200).json({ message: "Ordine ricevuto con successo!" });
   } else {
     res.status(405).end();
   }
